c50dab0ee4d299e057622eee710e16ca723efbb3
ray3347 <ansel.knb@gmail.com>
2023-05-02 14:50:01 +0700

    feat: added base template

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "./Test/Test";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload test 123.
         </p>
         <a
           className="App-link"
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Test/>
       </header>
     </div>
   );
---
+++ src/Test/Test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Test() {
+  return (
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  );
+}
+
+export default Test;
